dad80227746ae334313910eb3428dcd286f7b89f
DedAzaMarks <maxmutant1@yandex.ru>
2021-05-25 20:14:28 +0300

    file rebuild in 'NEW FIE'

--- Client.cpp
+++ Client.cpp
@@ -19,55 +19,28 @@ public:
         chunk_size = ch_sz;
     }
 
-    void hash_tbl (std::string& str, size_t from) {
+    void hash_tbl (std::string& str, size_t from, uint32_t shift) {
         std::string substr = str.substr(0, from);
         if (from != 0) {
-            R[xxh::xxhash<64>(substr)] = 0;
-            H[hash_h(substr)] = 0;
+            R[xxh::xxhash<64>(substr)] = 0 + shift / chunk_size;
+            H[hash_h(substr)] = 0 + shift / chunk_size;
         }
 
         size_t i = 0;
         for (i = from; i < str.size(); i += chunk_size) {
             substr = str.substr(i, std::min<uint32_t>(chunk_size, str.size() - i));
 
-            R[xxh::xxhash<64>(substr)] = i / chunk_size;
-            H[hash_h(substr)] = i / chunk_size;
+            R[xxh::xxhash<64>(substr)] = i / chunk_size + shift;
+            H[hash_h(substr)] = i / chunk_size + shift / chunk_size;
         }
         if (i > str.size()) {
             substr = str.substr(i - chunk_size, str.size());
-            R[xxh::xxhash<64>(substr)] = i / chunk_size - 1;
-            H[hash_h(substr)] = i / chunk_size - 1;//chunk_size;
+            R[xxh::xxhash<64>(substr)] = i / chunk_size - 1 + shift;
+            H[hash_h(substr)] = i / chunk_size - 1 + shift / chunk_size;//chunk_size;
         }
     }
 
 
-    //void reconstruct_data(DiffData data) {
-    //    std::string new_str;
-    //    size_t i = 0;
-    //    size_t j = 0;
-    //    while (i < data.data_blocks.size() && j < data.matched_blocks.size()) {
-    //        if (data.data_blocks[i].offset < data.matched_blocks[j].offset) {
-    //            new_str += data.data_blocks[i].data;
-    //            ++i;
-    //        }
-    //        else {
-    //            new_str += str.substr(data.matched_blocks[j].index * chunk_size,
-    //                                  std::min(chunk_size, str.size() - data.matched_blocks[j].index));
-    //            ++j;
-    //        }
-    //    }
-
-    //    while (i < data.data_blocks.size()) {
-    //        new_str += data.data_blocks[i].data;
-    //        ++i;
-    //    }
-    //    while (j < data.matched_blocks.size()) {
-    //        new_str += str.substr(data.matched_blocks[j].index * chunk_size,
-    //                              std::min(chunk_size, str.size() - data.matched_blocks[j].index));
-    //        ++j;
-    //    }
-    //    str = new_str;
-    //}
 
     //std::string to_string() {
     //    return str;
--- functions.cpp
+++ functions.cpp
@@ -1,5 +1,7 @@
 #include <fstream>
 #include <memory>
+#include <fcntl.h>
+#include <unistd.h>
 
 #include "Client.cpp"
 #include "types.h"
@@ -8,16 +10,17 @@ Client file_to_hash(std::ifstream& file, uint32_t chunk_size, size_t bufferSize)
     std::unique_ptr<char[]> buffer(new char[bufferSize]);
     uint32_t sz = 0;
     Client client(chunk_size);
+    uint32_t page = 0;
     while (file) {
         file.read(buffer.get(), bufferSize);
         sz = file.gcount();
         std::string str(buffer.get(), sz);
-        client.hash_tbl(str, 0);
+        client.hash_tbl(str, 0, page * bufferSize);
     }
     return client;
 }
 
-void compute_diff(DiffData& dd, Client& client, std::string& str) {
+void compute_diff(DiffData& dd, Client& client, std::string& str, uint32_t page_offset) {
     std::string substr = "";
     size_t data_offset = 0;
     size_t last = 0;
@@ -33,13 +36,14 @@ void compute_diff(DiffData& dd, Client& client, std::string& str) {
                 size_t index = client.H[h];
                 int overlap = 0;
                 if (last <= i) {
-                    dd.matched_blocks.push_back({index, i});
+                    dd.matched_blocks.push_back({index, i + page_offset});
                     last = i + chunk;
                 } else {
                     overlap = 1;
                 }
                 if (i > data_offset)
-                    dd.data_blocks.push_back({data_offset, str.substr(data_offset, i - data_offset)});
+                    dd.data_blocks.push_back({data_offset + page_offset,
+                            str.substr(data_offset, i - data_offset)});
                 if (overlap == 0)
                     data_offset = i + chunk;
             }
@@ -47,4 +51,62 @@ void compute_diff(DiffData& dd, Client& client, std::string& str) {
     }
     if (i != data_offset)
         dd.data_blocks.push_back({data_offset, str.substr(data_offset, i - data_offset)});
+}
+
+void reconstruct_data(char* filename, DiffData& data,
+                      uint32_t chunk_size, size_t bufferSize) {
+    std::string new_str;
+    std::ofstream out("NEW FILE", std::ios::out | std::ios::binary | std::ios::trunc);
+    size_t i = 0;
+    size_t j = 0;
+    while (i < data.data_blocks.size() && j < data.matched_blocks.size()) {
+        if (data.data_blocks[i].offset < data.matched_blocks[j].offset) {
+            out.write(data.data_blocks[i].data.data(), data.data_blocks[i].data.size());
+            ++i;
+        }
+        else {
+            int fd = open(filename, O_RDONLY);
+            size_t file_sz = lseek(fd, 0L, SEEK_END);
+            uint32_t sz;
+            if ((file_sz / bufferSize) * bufferSize >  data.matched_blocks[j].index * chunk_size) {
+                sz = std::min<uint32_t>(chunk_size,
+                                        (file_sz / bufferSize) * bufferSize - data.matched_blocks[j].index * chunk_size);
+            } else {
+                sz = std::min<uint32_t>(chunk_size, file_sz - data.matched_blocks[j].index * chunk_size);
+            }
+            std::unique_ptr<char[]> buf(new char[sz]);
+            lseek(fd, data.matched_blocks[j].index * chunk_size, SEEK_SET);
+            read(fd, buf.get(), sz);
+            out.write(buf.get(), sz);
+            close(fd);
+            //new_str += str.substr(y,
+            //                      std::min(chunk_size, str.size() - data.matched_blocks[j].index));
+            ++j;
+        }
+    }
+
+    while (i < data.data_blocks.size()) {
+        out.write(data.data_blocks[i].data.data(), data.data_blocks[i].data.size());
+        ++i;
+    }
+    while (j < data.matched_blocks.size()) {
+        int fd = open(filename, O_RDONLY);
+        size_t file_sz = lseek(fd, 0, SEEK_END);
+        uint32_t sz;
+        if ((file_sz / bufferSize) * bufferSize >  data.matched_blocks[j].index * chunk_size) {
+            sz = std::min<uint32_t>(chunk_size,
+                                    (file_sz / bufferSize) * bufferSize - data.matched_blocks[j].index * chunk_size);
+        } else {
+            sz = std::min<uint32_t>(chunk_size, file_sz - data.matched_blocks[j].index * chunk_size);
+        }
+        std::unique_ptr<char[]> buf(new char[sz]);
+        lseek(fd, data.matched_blocks[j].index * chunk_size, SEEK_SET);
+
+        read(fd, buf.get(), sz);
+        out.write(buf.get(), sz);
+        close(fd);
+        ++j;
+    }
+    // WRITE FILE
+    out.close();
 }
